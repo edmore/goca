527791f98d9ea834deea0fe30512c9c4d6691c9d
Edmore Moyo <etmoyo@gmail.com>
2013-10-03 01:22:07 +0200

    first commit

---
+++ auth/auth.go
@@ -0,0 +1,80 @@
+// package auth - Digest Authentication
+// http://play.golang.org/p/ABoHSHoTmu
+
+package auth
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type Authorization struct {
+	Username, Password, Realm, NONCE, QOP, Opaque, Algorithm string
+}
+
+func GetAuthorization(username, password string, resp *http.Response) *Authorization {
+	header := resp.Header.Get("www-authenticate")
+	parts := strings.SplitN(header, " ", 2)
+	parts = strings.Split(parts[1], ", ")
+	fmt.Println("Parts: ", parts)
+	opts := make(map[string]string)
+
+	for _, part := range parts {
+		fmt.Println("Part: ", part)
+		vals := strings.SplitN(part, "=", 2)
+		key := vals[0]
+		val := strings.Trim(vals[1], "\",")
+		opts[key] = val
+	}
+
+	auth := Authorization{
+		username, password,
+		opts["realm"], opts["nonce"], opts["qop"], opts["opaque"], opts["algorithm"],
+	}
+	return &auth
+}
+
+func SetDigestAuth(r *http.Request, username, password string, resp *http.Response, nc int) *http.Request {
+	auth := GetAuthorization(username, password, resp)
+	auth_str := GetAuthString(auth, r.URL, r.Method, nc)
+	r.Header.Add("Authorization", auth_str)
+	return r
+}
+
+func GetAuthString(auth *Authorization, url *url.URL, method string, nc int) string {
+	a1 := auth.Username + ":" + auth.Realm + ":" + auth.Password
+	h := md5.New()
+	io.WriteString(h, a1)
+	ha1 := hex.EncodeToString(h.Sum(nil))
+
+	h = md5.New()
+	a2 := method + ":" + url.Path
+	io.WriteString(h, a2)
+	ha2 := hex.EncodeToString(h.Sum(nil))
+
+	nc_str := fmt.Sprintf("%08x", nc)
+	hnc := "MTM3MDgw"
+
+	respdig := fmt.Sprintf("%s:%s:%s:%s:%s:%s", ha1, auth.NONCE, nc_str, hnc, auth.QOP, ha2)
+	h = md5.New()
+	io.WriteString(h, respdig)
+	respdig = hex.EncodeToString(h.Sum(nil))
+
+	base := "username=\"%s\", realm=\"%s\", nonce=\"%s\", uri=\"%s\", response=\"%s\""
+	base = fmt.Sprintf(base, auth.Username, auth.Realm, auth.NONCE, url.Path, respdig)
+	if auth.Opaque != "" {
+		base += fmt.Sprintf(", opaque=\"%s\"", auth.Opaque)
+	}
+	if auth.QOP != "" {
+		base += fmt.Sprintf(", qop=\"%s\", nc=%s, cnonce=\"%s\"", auth.QOP, nc_str, hnc)
+	}
+	if auth.Algorithm != "" {
+		base += fmt.Sprintf(", algorithm=\"%s\"", auth.Algorithm)
+	}
+	return "Digest " + base
+}
---
+++ goca.go
@@ -0,0 +1,64 @@
+// Go(lang) Matterhorn CA
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/edmore/goca/auth"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Config struct {
+	AdminServerURL string
+	DigestUser     string
+	DigestPassword string
+}
+
+var config *Config
+
+func loadConfig() {
+	file, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Println("open config: ", err)
+	}
+
+	temp := new(Config)
+	if err = json.Unmarshal(file, temp); err != nil {
+		log.Println("parse config: ", err)
+	}
+	config = temp
+}
+
+func main() {
+	// Load config
+	loadConfig()
+	fmt.Println(config.AdminServerURL)
+
+	client := &http.Client{}
+	name, _ := os.Hostname()
+
+	// Register CA
+	req, err := http.NewRequest("POST", config.AdminServerURL+"/capture-admin/agents/goca"+"?address=http://"+name+":8080&state=idle", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("X-Requested-Auth", "Digest")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req = auth.SetDigestAuth(req, config.DigestUser, config.DigestPassword, resp, 1)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	fmt.Println("Request: ", req)
+	resp, err = client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print Schedule
+	select {}
+}
